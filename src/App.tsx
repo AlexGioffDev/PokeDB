ab96cff5b619aa40ad65b402df11149aa7ccf1c0
Alex <alex.gioffre_dev@outlook.it>
2022-11-05 16:34:49 +0100

    Styling Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header.Components';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header.Components";
 
-const App = () => (
-  <Header />
-)
+const App = () => <Header />;
 
 export default App;
