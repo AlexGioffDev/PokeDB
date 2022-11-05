9445ca7d0428f9c0f19891223ab2028192f69e04
Alex <alex.gioffre_dev@outlook.it>
2022-11-05 15:53:08 +0100

    Initial Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header.Components';
 
-function App() {
-  return (
-   <h1 className='text-3xl font-bold underline text-red-600'>
-    Simple React Typescript Tailwind
-   </h1>
-  );
-}
+const App = () => (
+  <Header />
+)
 
 export default App;
