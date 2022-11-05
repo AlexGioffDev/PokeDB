f5f362c191842d223737cf785e02133adb58fe2d
Alex <alex.gioffre_dev@outlook.it>
2022-11-05 16:39:38 +0100

    Create tailwind variables for colors palette pokemon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pokemon-primaryBlue': '#0A285F',
+        'pokemon-secondaryBlue': '#0075BE',
+        'pokemon-primaryYellow': '#D5A100',
+        'pokemon-secondaryYellow': '#FFCC00',
+      }
+    },
   },
   plugins: [],
 }
