c8b9a869a79ad293ce18cf0a621458f943da28af
Alex <alex.gioffre_dev@outlook.it>
2022-11-14 16:43:47 +0100

    Fix folders name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header.Components";
-import { MainPage } from "./pages/Main.Page";
+import Header from "./Components/Header/Header.Components";
+import { MainPage } from "./Pages/Main.Page";
 
 const App = () => {
   return (
--- src/pages/Main.Page.tsx
+++ src/pages/Main.Page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PokemonList } from "../components/PokemonList/PokemonList.Components";
+import { PokemonList } from "../Components/PokemonList/PokemonList.Components";
 import pokemons from "../utilies/Pokemon.mock.json";
 
 export const MainPage = () => {
