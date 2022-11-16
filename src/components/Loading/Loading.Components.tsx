69d46c862f89f801965f249c61b091276230de77
Alex <alex.gioffre_dev@outlook.it>
2022-11-16 17:04:49 +0100

    Lading done

---
+++ src/components/Loading/Loading.Components.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loadingCircle = {
+  display: "block",
+  width: "3rem",
+  height: "3rem",
+  backgroundColor: "#0A285F",
+  borderRadius: "50%",
+  margin: "0px 5px",
+};
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const loadingCircleVariants = {
+  start: {
+    y: "50%",
+  },
+  end: {
+    y: "150%",
+  },
+};
+
+const loadingCircleTransition = {
+  duration: 0.5,
+  yoyo: Infinity,
+  ease: "easeInOut",
+};
+
+export const Loading = () => {
+  return (
+    <motion.div
+      className="w-full h-[90vh] flex justify-center items-center"
+      variants={loadingContainerVariants}
+      initial="start"
+      animate="end"
+    >
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+    </motion.div>
+  );
+};
